15f1b968a0e0ee0e5ab1b621ef07c816faee9362
Wisotzky Sven <sven.wisotzky@nokia.com>
2024-06-14 21:29:18 +0200

    santized script-content.mjs for helloGraal

--- templates/helloGraal/script-content.mjs
+++ templates/helloGraal/script-content.mjs
@@ -6,8 +6,8 @@ let HashMap = classResolver.resolveClass("java.util.HashMap");
 let Arrays = Java.type("java.util.Arrays");
 
 export class IntentTypeHandler {
-  INTENT_TYPE_NAME = "{{ intent_type }}";
-  INTENT_ROOT = "{{ intent_type }}:{{ intent_type }}";
+  INTENT_TYPE_NAME = "hellograal";
+  INTENT_ROOT = "hellograal:hellograal";
 
   constructor() {
     logger.info("Creating IntentTypeHandler for "+this.INTENT_TYPE_NAME);
@@ -53,7 +53,7 @@ export class IntentTypeHandler {
   }
 
   /**
-    * Function to audit intents.
+    * Method to audit intents.
     * 
     * @param {AuditInput} input used to access information about the intent to be audited
     * @returns {AuditReport} audit-report object containing the misaligned attributed and objects 
@@ -69,9 +69,7 @@ export class IntentTypeHandler {
 
     const config = JSON.parse(input.getJsonIntentConfiguration())[0][this.INTENT_ROOT];
     var topology = input.getCurrentTopology();
-    //var report   = auditFactory.createAuditReport(null, null);
     var report   = new AuditReport();
-
 
     // misaligned object (is-configured=false)
     let misAlignedObject;
@@ -97,7 +95,7 @@ export class IntentTypeHandler {
 
   /**
    * Validation of intent config/target that is automatically called for intent 
-   * create/edit and state-change operations. This function may do enhanced
+   * create/edit and state-change operations. This method may do enhanced
    * validation, in addition to checks against the intent model (YANG), that is
    * automatically executed by the intent engine.
    * 
@@ -138,7 +136,7 @@ export class IntentTypeHandler {
   }
 
   /**
-   * Returns the list of managed devices (neId) for suggesting on WebUI
+   * Returns the list of managed devices (neId) for WebUI suggest
    * @param {ValueProviderContext} valueProviderContext
    * @returns Suggestion data in Map format
    */  
@@ -155,33 +153,28 @@ export class IntentTypeHandler {
     var rvalue = new HashMap();
     try  {
       // get connected mediators
-
       var mediators = [];
-      mds.getAllManagersOfType("REST").forEach(function(mediator) {
+      mds.getAllManagersOfType("REST").forEach((mediator) => {
         if (mds.getManagerByName(mediator).getConnectivityState().toString() === 'CONNECTED')
           mediators.push(mediator);
       });
 
       // get managed devices from mediator(s)
-
-      var devices = mds.getAllManagedDevicesFrom(Arrays.asList(mediators));
+      const devices = mds.getAllManagedDevicesFrom(Arrays.asList(mediators));
 
       // filter result by searchString provided from WebUI
-
       var filteredDevicenames = [];
-      devices.forEach(function(device) {
+      devices.forEach((device) => {
         if (!searchString || device.getName().indexOf(searchString) !== -1)
           filteredDevicenames.push(device.getName());
       });
       logger.info("Found "+filteredDevicenames.length+" devices!");
 
       // convert output into HashMap required by WebUI framework
-
       filteredDevicenames.sort().forEach((devicename) => rvalue[devicename]=devicename);
     }
     catch (e) {
       logger.error("Exception catched: " + e);
-
     }
 
     const duration = Date.now()-startTS;
