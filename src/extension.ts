2c8ab7927d937f9c3682252076df57ad8050aeb1
AbduAwad <abdulrahmansawad@gmailcom>
2024-07-04 10:05:37 -0400

    update to NSP connect. port compatibility

--- src/extension.ts
+++ src/extension.ts
@@ -30,26 +30,24 @@ export function activate(context: vscode.ExtensionContext) {
 	context.subscriptions.push(vscode.commands.registerCommand('nokia-intent-manager.newVersion',       async (...args) => imProvider.newVersion(args)));
 	context.subscriptions.push(vscode.commands.registerCommand('nokia-intent-manager.newIntentType',    async (...args) => imProvider.newIntentTypeFromTemplate(args)));
 	
-	vscode.commands.registerCommand('nokia-intent-manager.connect', async (username: string|undefined, password: string|undefined, nspAddr: string|undefined, standardPort: Boolean) => {
+	vscode.commands.registerCommand('nokia-intent-manager.connect', async (username: string|undefined, password: string|undefined, nspAddr: string|undefined, port: string) => {
 		const config = vscode.workspace.getConfiguration('intentManager');
-		if (username === undefined)
+		if (username === undefined) {
 			username = await vscode.window.showInputBox({title: "Username"});
-		if (username !== undefined)
-			config.update("user", username, vscode.ConfigurationTarget.Workspace);
-		if (password === undefined)
-			password = await vscode.window.showInputBox({password: true, title: "Password"});
-		if (password !== undefined)
-			secretStorage.store("nsp_im_password", password);
-		if (standardPort === true) {
-			config.update("port", "443", vscode.ConfigurationTarget.Workspace);
-		} else {
-			let imPort = await vscode.window.showInputBox({ prompt: 'Enter Port for NSP IM...' });
-			config.update("port", imPort, vscode.ConfigurationTarget.Workspace);
 		}
+		if (username !== undefined) {
+			config.update("user", username, vscode.ConfigurationTarget.Workspace);
+		}
+		if (password === undefined) {
+			password = await vscode.window.showInputBox({password: true, title: "Password"});
+		} else {
+			secretStorage.store("nsp_im_password", password);
+		}
+		config.update("port", port, vscode.ConfigurationTarget.Workspace);
 		config.update("NSPIP", nspAddr, vscode.ConfigurationTarget.Workspace);
 	});
 
-	function updateStatusBarItem(){
+	function updateStatusBarItem() {
 		const editor = vscode.window.activeTextEditor;
 		const sbar = imProvider.getStatusBarItem();
 
